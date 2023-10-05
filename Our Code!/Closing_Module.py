2d09a9fea74a5172d2064421b4597d38527d3846
AdamFaucher <ajfaucher@wpi.edu>
2023-10-05 10:38:30 -0400

    New Modules
    Created a module for the ChatGPT functions, a greeting function, and a closing function.

---
+++ Our Code!/Closing_Module.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  5 10:28:47 2023
+
+@author: adamfaucher
+"""
+
+def generate_closing_prof():
+    '''
+    A function that generates a professional "Sincerly, [USER NAME]" style closing to the email
+    
+    :Param
+    :Return
+    '''
---
+++ Our Code!/Greeting_Module.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  5 10:27:03 2023
+
+@author: adamfaucher
+"""
+
+def generate_greeting():
+    '''
+    A function that generates a professional "Hello, [SENDER NAME]," style greeting to the email
+    
+    :Param
+    :Return
+    '''
