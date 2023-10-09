3df0f3199f27d53c375d4532c055ec9e67d7fb71
Joseph Anthony Saladino <jasaladino@wpi.edu>
2023-10-09 09:46:41 -0400

    Updated to Greeting

--- Our Code!/Greeting_Module.py
+++ Our Code!/Greeting_Module.py
@@ -6,10 +6,21 @@ Created on Thu Oct  5 10:27:03 2023
 @author: adamfaucher
 """
 
-def generate_greeting():
+def generate_greeting(sender_name):
     '''
     A function that generates a professional "Hello, [SENDER NAME]," style greeting to the email
-    
-    :Param
-    :Return
+        
+    :Param string of the email sender's name'
+    :Return larger string of the email sender's with the greeting'
     '''
+    
+    if 'Professor' in sender_name:
+        greeting_string = 'Dear, ' + sender_name
+        sender_type_Prof = True
+    if 'Dr' in sender_name:
+        greeting_string = 'Dear, ' + sender_name
+        sender_type_Prof = True
+    elif 'Professor' or 'Dr' not in sender_name:
+        greeting_string = 'Hello, ' + sender_name    
+        sender_type_Prof = False
+    return (greeting_string, sender_type_Prof)
