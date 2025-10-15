c2dc125ede0070c5c10bc366addd4e9bd164a5cc
AdamFaucher <143050473+AdamFaucher@users.noreply.github.com>
2025-10-15 10:42:31 -0400

    Update ChatGPT_Module2.py

--- Our Code/ChatGPT_Module2.py
+++ Our Code/ChatGPT_Module2.py
@@ -1,6 +1,6 @@
 import openai
 
-secret_key = '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'
+secret_key = ''
 openai.api_key = secret_key
 openai.api_key_path = "/Users/adamfaucher/Documents/GitHub/Auto-Email-Response-Final-Project/Our Code/key.txt"
 #ChatGPT was used to generate some of this code https://chat.openai.com/
